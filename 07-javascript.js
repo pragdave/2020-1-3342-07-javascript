a51a439bda0ed7ee6b470d54028b994eaba18583
CeeCeeLemons <corey.clemons@swbell.net>
2020-03-05 23:21:15 -0600

    My Submission

--- 07-javascript.js
+++ 07-javascript.js
@@ -67,6 +67,9 @@ let str4 = "briefly"
 let str5 = "Offensive Word"
 
 // START
+String.prototype.bowlderize = function () {
+    return this.replace(/[aeiou]/ig, "*")
+}
 // END
 
 assert.equal(str1.bowlderize(), "*bc")
@@ -74,8 +77,6 @@ assert.equal(str2.bowlderize(), "*BC")
 assert.equal(str3.bowlderize(), "b**f")
 assert.equal(str4.bowlderize(), "br**fly")
 assert.equal(str5.bowlderize(), "*ff*ns*v* W*rd")
-
-if (false) {
 
 ///////////////// Section 2
 //
@@ -95,6 +96,14 @@ if (false) {
 //
 
 // START
+function Person(name, title) {
+    this.name = name
+    this.title = title
+}
+
+Person.prototype.fullName = function () {
+    return `${this.title} ${this.name}`
+}
 // END
 
 p = new Person("Betty", "Ms")
@@ -115,6 +124,15 @@ assert.equal(p.fullName(), "Ms Betty")
 //
 
 //START
+class Person1 {
+    constructor(name, title) {
+        this.name = name
+        this.title = title
+    }
+    fullName() {
+        return `${this.title} ${this.name}`
+    }
+}
 // END
 
 p = new Person1("Fred", "Mr")
@@ -143,6 +161,21 @@ assert(p.hasOwnProperty("name"))
 // Penalty: -3 layout, -3 naming
 
 //START
+function bugs(reducer) {
+    temp = Object.create({ sup: String.prototype.sup })
+    String.prototype.sup = function () {
+        return `What's up, ${this}?`;
+    }
+    try {
+        reducer()
+    }
+    catch (err) {
+        throw err
+    }
+    finally {
+        String.prototype.sup = temp.sup
+    }
+}
 //END
 
 assert.equal("doc".sup(), "<sup>doc</sup>")
@@ -173,7 +206,6 @@ assert.throws(
 
 assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
-
 ///////////////// Section 5
 //
 // We talked about what the `new` operator does.
@@ -192,8 +224,11 @@ assert.equal("DOC".sup(), "<sup>DOC</sup>")
 // Penalty: -2 layout, -2 naming, -5 works but limited
 
 function myNew(constructor, ...args) {
-  //START
-  //END
+//START
+    ret = Object.create(constructor.prototype)
+    ret.constructor(...args)
+    return ret
+//END
 }
 
 function Box(w, h) {
@@ -208,4 +243,6 @@ box = myNew(Box, 5, 7)
 assert.equal(box.w, 5)
 assert.equal(box.h, 7)
 assert.equal(box.area(), 35)
+
+if (false) {
 }
