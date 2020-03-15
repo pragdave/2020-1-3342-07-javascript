912de63de3ad1bcc1f153fee962fb644d5b31c61
Daniel Dewan <danieldewan@Daniels-MacBook-Pro-216.local>
2020-03-15 16:04:15 -0500

    All tests passed

--- 07-javascript.js
+++ 07-javascript.js
@@ -67,6 +67,9 @@ let str4 = "briefly"
 let str5 = "Offensive Word"
 
 // START
+String.prototype.bowlderize = function() {
+  return this.replace(/[aeiou]/ig,'*')
+}
 // END
 
 assert.equal(str1.bowlderize(), "*bc")
@@ -75,7 +78,7 @@ assert.equal(str3.bowlderize(), "b**f")
 assert.equal(str4.bowlderize(), "br**fly")
 assert.equal(str5.bowlderize(), "*ff*ns*v* W*rd")
 
-if (false) {
+
 
 ///////////////// Section 2
 //
@@ -95,6 +98,14 @@ if (false) {
 //
 
 // START
+function Person(name, title) {
+  this.name = name
+  this.title = title
+}
+
+Person.prototype.fullName = function() {
+  return `${this.title} ${this.name}`
+}
 // END
 
 p = new Person("Betty", "Ms")
@@ -115,6 +126,15 @@ assert.equal(p.fullName(), "Ms Betty")
 //
 
 //START
+class Person1 {
+  constructor(name, title) {
+    this.name = name
+    this.title = title
+  }
+  fullName() {
+    return `${this.title} ${this.name}`
+  }
+}
 // END
 
 p = new Person1("Fred", "Mr")
@@ -143,6 +163,18 @@ assert(p.hasOwnProperty("name"))
 // Penalty: -3 layout, -3 naming
 
 //START
+function bugs(func) {
+  let sup = String.prototype.sup
+  String.prototype.sup = function() {
+    return `What's up, ${this}?`
+  }
+  try {
+    func()
+  }
+  finally {
+    String.prototype.sup = sup
+  }
+}
 //END
 
 assert.equal("doc".sup(), "<sup>doc</sup>")
@@ -193,6 +225,9 @@ assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
 function myNew(constructor, ...args) {
   //START
+  let newBox = Object.create(constructor.prototype)
+  newBox.constructor(...args)
+  return newBox
   //END
 }
 
@@ -208,4 +243,5 @@ box = myNew(Box, 5, 7)
 assert.equal(box.w, 5)
 assert.equal(box.h, 7)
 assert.equal(box.area(), 35)
+if (false) {
 }
