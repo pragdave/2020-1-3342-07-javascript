d0053084ffa5c713343510bc120158cfa34f9f91
AbrahamA00 <46973419+AbrahamA00@users.noreply.github.com>
2020-03-15 19:35:31 -0400

    All tests pass

--- 07-javascript.js
+++ 07-javascript.js
@@ -67,6 +67,20 @@ let str4 = "briefly"
 let str5 = "Offensive Word"
 
 // START
+String.prototype.bowlderize = function () {
+  let output = ""
+  for(let i = 0; i < this.length; i++)
+  {
+    if(isVowel(this.charAt(i)))
+    {
+      output += "*"
+    }
+    else
+      output += this.substring(i, i + 1)
+  }
+  return output
+}
+function isVowel(x) {  return /[aeiouAEIOU]/.test(x); } // Code borrowed from https://stackoverflow.com/questions/26926994/javascript-check-if-character-is-a-vowel
 // END
 
 assert.equal(str1.bowlderize(), "*bc")
@@ -75,7 +89,7 @@ assert.equal(str3.bowlderize(), "b**f")
 assert.equal(str4.bowlderize(), "br**fly")
 assert.equal(str5.bowlderize(), "*ff*ns*v* W*rd")
 
-if (false) {
+
 
 ///////////////// Section 2
 //
@@ -95,6 +109,14 @@ if (false) {
 //
 
 // START
+function Person(name = "", title = ""){
+    this.name = name
+    this.title = title
+}
+
+Person.prototype.fullName =  function() {
+  return this.title + " " + this.name
+}
 // END
 
 p = new Person("Betty", "Ms")
@@ -115,6 +137,15 @@ assert.equal(p.fullName(), "Ms Betty")
 //
 
 //START
+class Person1{
+  constructor(name = "", title = "") {
+    this.name = name
+    this.title = title
+  } 
+  fullName = function() {
+    return this.title + " " + this.name
+  }
+}
 // END
 
 p = new Person1("Fred", "Mr")
@@ -143,6 +174,19 @@ assert(p.hasOwnProperty("name"))
 // Penalty: -3 layout, -3 naming
 
 //START
+function bugs(sups) {
+  let original = String.prototype.sup
+  String.prototype.sup = function(){
+    let modified = "What's up, " + this + "?"
+    return modified
+  }
+  try{
+    sups()
+  }
+  finally{
+      String.prototype.sup = original
+    }
+}
 //END
 
 assert.equal("doc".sup(), "<sup>doc</sup>")
@@ -173,7 +217,6 @@ assert.throws(
 
 assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
-
 ///////////////// Section 5
 //
 // We talked about what the `new` operator does.
@@ -193,6 +236,9 @@ assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
 function myNew(constructor, ...args) {
   //START
+  let thisObj = Object.create(constructor.prototype)
+  constructor.apply(thisObj, args)
+  return thisObj
   //END
 }
 
@@ -208,4 +254,5 @@ box = myNew(Box, 5, 7)
 assert.equal(box.w, 5)
 assert.equal(box.h, 7)
 assert.equal(box.area(), 35)
+if (false) {
 }
