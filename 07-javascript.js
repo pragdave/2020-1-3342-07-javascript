4b021a80519fafdaa7e81fd51fca5b7b0adc5610
qinyigou <zhenbangz@smu.edu>
2020-03-15 19:46:40 -0500

    test2

--- 07-javascript.js
+++ 07-javascript.js
@@ -67,15 +67,21 @@ let str4 = "briefly"
 let str5 = "Offensive Word"
 
 // START
+  function bowlderize(nStr){
+    this.nStr = nStr;
+    var strNew = nStr + "";
+    strNew = strNew.replace(/a|e|i|o|u/gi,"*");
+    return strNew;
+  }
 // END
 
-assert.equal(str1.bowlderize(), "*bc")
-assert.equal(str2.bowlderize(), "*BC")
-assert.equal(str3.bowlderize(), "b**f")
-assert.equal(str4.bowlderize(), "br**fly")
-assert.equal(str5.bowlderize(), "*ff*ns*v* W*rd")
+assert.equal(bowlderize(str1), "*bc")
+assert.equal(bowlderize(str2), "*BC")
+assert.equal(bowlderize(str3), "b**f")
+assert.equal(bowlderize(str4), "br**fly")
+assert.equal(bowlderize(str5), "*ff*ns*v* W*rd")
 
-if (false) {
+
 
 ///////////////// Section 2
 //
@@ -95,6 +101,17 @@ if (false) {
 //
 
 // START
+function Person(iName, iTitle){
+  
+  this.name = iName;
+  this.title = iTitle;
+  
+}
+
+Person.prototype.fullName = function(){
+  return this.title + " " + this.name;
+}
+
 // END
 
 p = new Person("Betty", "Ms")
@@ -115,6 +132,15 @@ assert.equal(p.fullName(), "Ms Betty")
 //
 
 //START
+class Person1{
+  constructor(name,title){
+    this.name = name;
+    this.title = title;
+  }
+  fullName(){
+    return this.title + " " + this.name;
+  }
+}
 // END
 
 p = new Person1("Fred", "Mr")
@@ -143,6 +169,14 @@ assert(p.hasOwnProperty("name"))
 // Penalty: -3 layout, -3 naming
 
 //START
+function bugs(aFunction){
+  this.aFunction = aFunction;
+    var newName1 = aFunction + "";
+    var n = newName1.length;
+    var newName1 = newName1.substr(5,n-11);
+    return "What's up, " + newName1 + "?";
+}
+
 //END
 
 assert.equal("doc".sup(), "<sup>doc</sup>")
@@ -151,6 +185,8 @@ bugs(function() {
   assert.equal("Dave".sup(), "What's up, Dave?")
 })
 assert.equal("DOC".sup(), "<sup>DOC</sup>")
+
+
 
 
 // this second test makes sure that you are correctly
@@ -173,6 +209,7 @@ assert.throws(
 
 assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
+if (false) {
 
 ///////////////// Section 5
 //
@@ -193,6 +230,10 @@ assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
 function myNew(constructor, ...args) {
   //START
+  let obj ={};
+  obj.__proto__ = constructor.prototype;
+  let result = constructor.call(obj, ...args);
+  return result instanceof Object ? result : obj;
   //END
 }
 
@@ -208,4 +249,5 @@ box = myNew(Box, 5, 7)
 assert.equal(box.w, 5)
 assert.equal(box.h, 7)
 assert.equal(box.area(), 35)
+
 }
