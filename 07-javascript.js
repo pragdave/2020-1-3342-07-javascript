52cd6ee31e9e7f3bad3ce14d5463fc2fef0efe85
Nicholas <nylu@smu.edu>
2020-03-15 19:49:42 -0500

    Section 1 works

--- 07-javascript.js
+++ 07-javascript.js
@@ -67,6 +67,10 @@ let str4 = "briefly"
 let str5 = "Offensive Word"
 
 // START
+String.prototype.bowlderize = function () {
+  var s = this.replace(/[aeiouAEIOU]/g, "*")
+  return s
+}
 // END
 
 assert.equal(str1.bowlderize(), "*bc")
@@ -77,135 +81,136 @@ assert.equal(str5.bowlderize(), "*ff*ns*v* W*rd")
 
 if (false) {
 
-///////////////// Section 2
-//
-// Write a constructor function and any associated code
-// to implement a Person class. The constructor takes a name
-// and a title (Ms, Mr, ...) both of which are stored in the resulting
-// object. The object will also have a `fullName()` function
-// that returns the concatenation of the title and the name
-//
-// The tests show this in operation.
-//
-// It is OK to add the `fullName` function directly to the prototype.
-//
-// Grading: 20 for passing the tests
-// Penalty:  -3 for layout
-//           -3 for bad names
-//
+  ///////////////// Section 2
+  //
+  // Write a constructor function and any associated code
+  // to implement a Person class. The constructor takes a name
+  // and a title (Ms, Mr, ...) both of which are stored in the resulting
+  // object. The object will also have a `fullName()` function
+  // that returns the concatenation of the title and the name
+  //
+  // The tests show this in operation.
+  //
+  // It is OK to add the `fullName` function directly to the prototype.
+  //
+  // Grading: 20 for passing the tests
+  // Penalty:  -3 for layout
+  //           -3 for bad names
+  //
 
-// START
-// END
+  // START
+  // END
 
-p = new Person("Betty", "Ms")
-assert.equal(p.name,  "Betty")
-assert.equal(p.title, "Ms")
-assert.equal(p.fullName(), "Ms Betty")
-
-
-///////////////// Section 3
-//
-// Reimplement the code from section 2 using the ES2015
-// `class` syntax. Call the class `Person1` so the name
-// doesn't clash with that section.
-//
-// Grading: 15 for passing the tests
-// Penalty:  -2 for layout
-//           -2 for bad names
-//
-
-//START
-// END
-
-p = new Person1("Fred", "Mr")
-assert.equal(p.name,  "Fred")
-assert.equal(p.title, "Mr")
-assert.equal(p.fullName(), "Mr Fred")
-assert(p.hasOwnProperty("name"))
+  p = new Person("Betty", "Ms")
+  assert.equal(p.name, "Betty")
+  assert.equal(p.title, "Ms")
+  assert.equal(p.fullName(), "Ms Betty")
 
 
-///////////////// Section 4
-//
-// The built-in String class defines an instance method
-// `sup()` that wraps the string in HTML <sup> tags.
-//
-// Write a function called `bugs` that takes a function as
-// a parameter. This function should change the way
-// `String.sup()` works: rather than "Doc".sup() returning
-// "<sup>Doc</sup>", it should return "What's up, Doc?".
-// It should then call the function that was passed in. When
-// that function returns, it should reset String so that
-// the original `sup()` function is restored.
-//
-// (ps. This is a bad idea in real code)
-//
-// Grading: 20 points to pass tests
-// Penalty: -3 layout, -3 naming
+  ///////////////// Section 3
+  //
+  // Reimplement the code from section 2 using the ES2015
+  // `class` syntax. Call the class `Person1` so the name
+  // doesn't clash with that section.
+  //
+  // Grading: 15 for passing the tests
+  // Penalty:  -2 for layout
+  //           -2 for bad names
+  //
 
-//START
-//END
+  //START
+  // END
 
-assert.equal("doc".sup(), "<sup>doc</sup>")
-bugs(function() {
-  assert.equal("Doc".sup(), "What's up, Doc?")
-  assert.equal("Dave".sup(), "What's up, Dave?")
-})
-assert.equal("DOC".sup(), "<sup>DOC</sup>")
+  p = new Person1("Fred", "Mr")
+  assert.equal(p.name, "Fred")
+  assert.equal(p.title, "Mr")
+  assert.equal(p.fullName(), "Mr Fred")
+  assert(p.hasOwnProperty("name"))
 
 
-// this second test makes sure that you are correctly
-// restoring the `sup()` function if the function passed
-// to `bugs()` throws an exception. You might need to
-// investigate JavaScript exception handling and the
-// `finally` clause.
+  ///////////////// Section 4
+  //
+  // The built-in String class defines an instance method
+  // `sup()` that wraps the string in HTML <sup> tags.
+  //
+  // Write a function called `bugs` that takes a function as
+  // a parameter. This function should change the way
+  // `String.sup()` works: rather than "Doc".sup() returning
+  // "<sup>Doc</sup>", it should return "What's up, Doc?".
+  // It should then call the function that was passed in. When
+  // that function returns, it should reset String so that
+  // the original `sup()` function is restored.
+  //
+  // (ps. This is a bad idea in real code)
+  //
+  // Grading: 20 points to pass tests
+  // Penalty: -3 layout, -3 naming
 
-
-assert.throws(
-  () => {
-  bugs(function() {
-    throw new Error("boom")
-  })},
-  {
-    name: "Error",
-    message: "boom"
-  }
-)
-
-assert.equal("DOC".sup(), "<sup>DOC</sup>")
-
-
-///////////////// Section 5
-//
-// We talked about what the `new` operator does.
-//
-// Write your own version `myNew` that takes a constructor
-// function and any arguments, and that sets up the environment that allows that
-// constructor function to run (setting `this`, and returning that `this` value
-// to the caller.
-//
-// This only needs to work on constructor functions: you don;t need to consider
-// the `class` syntax.
-//
-// Hint: it's 3 lines of code
-//
-// Grade: 15
-// Penalty: -2 layout, -2 naming, -5 works but limited
-
-function myNew(constructor, ...args) {
   //START
   //END
-}
 
-function Box(w, h) {
-  this.w = w
-  this.h = h
-}
-Box.prototype.area = function() {
-  return this.w*this.h
-}
+  assert.equal("doc".sup(), "<sup>doc</sup>")
+  bugs(function () {
+    assert.equal("Doc".sup(), "What's up, Doc?")
+    assert.equal("Dave".sup(), "What's up, Dave?")
+  })
+  assert.equal("DOC".sup(), "<sup>DOC</sup>")
 
-box = myNew(Box, 5, 7)
-assert.equal(box.w, 5)
-assert.equal(box.h, 7)
-assert.equal(box.area(), 35)
+
+  // this second test makes sure that you are correctly
+  // restoring the `sup()` function if the function passed
+  // to `bugs()` throws an exception. You might need to
+  // investigate JavaScript exception handling and the
+  // `finally` clause.
+
+
+  assert.throws(
+    () => {
+      bugs(function () {
+        throw new Error("boom")
+      })
+    },
+    {
+      name: "Error",
+      message: "boom"
+    }
+  )
+
+  assert.equal("DOC".sup(), "<sup>DOC</sup>")
+
+
+  ///////////////// Section 5
+  //
+  // We talked about what the `new` operator does.
+  //
+  // Write your own version `myNew` that takes a constructor
+  // function and any arguments, and that sets up the environment that allows that
+  // constructor function to run (setting `this`, and returning that `this` value
+  // to the caller.
+  //
+  // This only needs to work on constructor functions: you don;t need to consider
+  // the `class` syntax.
+  //
+  // Hint: it's 3 lines of code
+  //
+  // Grade: 15
+  // Penalty: -2 layout, -2 naming, -5 works but limited
+
+  function myNew(constructor, ...args) {
+    //START
+    //END
+  }
+
+  function Box(w, h) {
+    this.w = w
+    this.h = h
+  }
+  Box.prototype.area = function () {
+    return this.w * this.h
+  }
+
+  box = myNew(Box, 5, 7)
+  assert.equal(box.w, 5)
+  assert.equal(box.h, 7)
+  assert.equal(box.area(), 35)
 }
